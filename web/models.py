07a8464f8b46a40a6b96bcd8ba3f3a448474b106
ngothikimoanh <ntkoanh.orin@outlook.com>
2025-07-27 20:43:13 +0700

    Create user models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
 from django.db import models
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, username, password, **extra_fields):
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, password, **extra_fields)
+
+class User(AbstractBaseUser,PermissionsMixin):
+    username= models.CharField(max_length=16, unique= True)
+    is_staff= models.BooleanField(default=False)
+    name = models.CharField(max_length=16)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "username"
+
+    class Meta:
+        db_table = "users"
