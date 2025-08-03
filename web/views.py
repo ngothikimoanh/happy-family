32abc95b38780899eb7eb5d73047cd85b202a985
ngothikimoanh <ntkoanh.orin@outlook.com>
2025-08-03 15:42:57 +0700

    Create register form

--- web/views.py
+++ web/views.py
@@ -3,11 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request):
     return render(request, "pages/home.html")
+
 def cost_view(request):
     return render(request, "pages/cost.html")
+
 def humansource_view(request):
     return render(request, "pages/humansource.html")
+
 def calendar_view(request):
     return render(request, "pages/calendar.html")
+
 def storage_view(request):
     return render(request, "pages/storage.html")
+
+def register_view(request):
+    return render(request, "pages/register.html")
